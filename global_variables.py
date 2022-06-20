bfa267a80e0757f9181a90782eecb4a80dd015a1
Shariful Islam <shariful0804@gmail.com>
2022-06-20 16:43:20 +0600

    type is added

---
+++ global_variables.py
@@ -0,0 +1,21 @@
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+y = "awesome"
+
+def myfunc():
+  print("Python is " + y)
+
+myfunc()
+
+global z
+z=7
+def myglobal():
+   print(z)
